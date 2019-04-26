3256d9e8e7cf7ec5227d42783fde0f9cd07ad920
kongacute <thaibinh262@gmail.com>
2019-04-26 17:07:43 +0700

    Change index.php file to template file
    Just the template

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World :D</p>'; ?> 
- </body>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php 
+$sql = "SELECT id, name FROM label";
+
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+
+foreach ($resultSet as $row) {
+	echo $row['name'] . '\n';
+}
+
+?>
+</body>
 </html>
