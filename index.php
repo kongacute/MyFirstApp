cdb23971fc4de2b78382b953be9caf7bdceb343f
kongacute <thaibinh262@gmail.com>
2019-05-05 21:26:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -80,17 +80,7 @@ span.psw {
 
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
-    <?php
-    session_start();
-    $first = true;
-    $_SESSION['$first'] = $first;
-    if (!$first) {?>
-    <label>Wrong user name or password! Please check again!</label>
-    }?>    
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
+  
   </div>
 
   <div class="container" style="background-color:#f1f1f1">
