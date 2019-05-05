73362ee22499d7ed2f3ff4fc324d0dc5e702d765
kongacute <thaibinh262@gmail.com>
2019-05-05 21:22:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -72,6 +72,8 @@ span.psw {
 <h2>Login Form</h2>
 
 <form action="/adminpage.php" method="post">
+  <div class="imgcontainer">
+  </div>
   <div class="container">
     <label for="uname"><b>Username</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
@@ -84,6 +86,9 @@ span.psw {
     <label>Wrong user name or password! Please check again!</label>
     }?>    
     <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
   </div>
 
   <div class="container" style="background-color:#f1f1f1">
