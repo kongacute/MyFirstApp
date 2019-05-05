e1248aff1a53003a0bceb4905b0ce7433ccce5bb
kongacute <thaibinh262@gmail.com>
2019-05-05 14:45:15 +0700

    Update adminpage.php

--- adminpage.php
+++ adminpage.php
@@ -9,10 +9,10 @@ $db = pg_connect($db_url);
 $uname = $_POST['uname'];
 $pwd = $_POST['pwd'];
 
-$checkuser = "SELECT COUNT(*) INTO 'check_user' FROM customer WHERE 'User Name' = '$uname' AND 'Password' = '$pwd'";
-$checkadmin = "SELECT COUNT(*) INTO 'check_admin' FROM admin WHERE 'User Name' = '$uname' AND 'Password' = '$pwd'";
-$delete_admin_check = "DROP TABLE IF EXISTS 'check_admin'";
-$delete_user_check = "DROP TABLE IF EXISTS 'check_user'";
+$checkuser = "SELECT COUNT(*) INTO check_user FROM customer WHERE 'User Name' = '$uname' AND 'Password' = '$pwd'";
+$checkadmin = "SELECT COUNT(*) INTO check_admin FROM admin WHERE 'User Name' = '$uname' AND 'Password' = '$pwd'";
+$delete_admin_check = "DROP TABLE IF EXISTS check_admin";
+$delete_user_check = "DROP TABLE IF EXISTS check_user";
 $queryadmin = "SELECT * FROM customer";
 $login_check_admin = pg_query($db, $checkadmin);
 $login_check_user = pg_query($db, $checkuser);
