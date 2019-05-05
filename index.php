aa566a11b14819334e50b5412ed9c8b4c6254f27
kongacute <thaibinh262@gmail.com>
2019-05-05 11:00:03 +0700

    Delete useless function

--- index.php
+++ index.php
@@ -86,10 +86,6 @@ span.psw {
     <label>Wrong user name or password! Please check again!</label>
     }?>    
     <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
 
   <div class="container" style="background-color:#f1f1f1">
     <button type="button" class="cancelbtn">Cancel</button>
