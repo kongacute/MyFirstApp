667cea8536c946a19c24a7ca477b7db2f38c7a3d
kongacute <thaibinh262@gmail.com>
2019-05-05 21:45:45 +0700

    Update adminpage.php

--- adminpage.php
+++ adminpage.php
@@ -1,4 +1,14 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Seller - ATN Company</title>"
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php
 $db_url = getenv("DATABASE_URL") ?: "postgres://stejeexemgbraf:a5a875444f2192a1fb8982b181a046ce7c194400d26f9c583934ddb28d6a7b80@ec2-50-19-127-115.compute-1.amazonaws.com:5432/d4brobjaq8sj8t";
 
@@ -40,16 +50,6 @@ else if ((int)$row_user['check_user'] >= 1)
 $delete_admin = pg_query($db, $delete_admin_check);
 $delete_user = pg_query($db, $delete_user_check);
 ?>
-<html lang="en">
-<head>
-  <title>Seller - ATN Company</title>"
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
-<body>
 <div class="container">
   <h2>Customers List</h2>
   <p>List of customers</p>            
