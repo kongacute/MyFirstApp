07c626dd722737db4949943cce32868cec7a6590
kongacute <thaibinh262@gmail.com>
2019-05-06 20:57:43 +0700

    Update adminpage.php

--- adminpage.php
+++ adminpage.php
@@ -63,7 +63,8 @@ if (pg_num_rows($resultcheckuser)>0)
         echo "<td>" . $add. "</td>";
         echo "<td>" . $city . "</td>";
         echo "<td>" . $region . "</td>";
-        echo "<td>" . $phone . "</td>";  
+        echo "<td>" . $phone . "</td>"; 
+        echo pg_num_rows($resultcheckuser); 
         echo "<tr></tr>";
     }
       ?>
